8de57d63f5b3e91b7b26480f482c1bb0b3cdef7c
aelsammak <adielsammak@cmail.carleton.ca>
2022-02-22 14:04:38 -0500

    Provided the POST endpoint to REST controller necessary for a surveyor to create a survey. Additionally, added the necessary service & repo methods/classes.

--- src/main/java/ca/group20/sysc4806project/repository/SurveyorRepo.java
+++ src/main/java/ca/group20/sysc4806project/repository/SurveyorRepo.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SurveyorRepo extends JpaRepository<Surveyor, Long> {
-    Surveyor findByUsername(String username);
+    Surveyor findByName(String surveyorName);
 }
